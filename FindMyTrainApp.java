2231c3958934d680c59b9252cd15ebdd59e69679
Liladhar Meshram <liladharmeshram650@gmail.com>
2024-10-29 20:35:25 +0530

    OOPS_Project

---
+++ FindMyTrainApp.java
@@ -0,0 +1,51 @@
+package FindMyTrain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FindMyTrainApp {
+    public static void main(String[] args) {
+        TrainSystem system = new TrainSystem();
+
+        // adding new station;
+        Station station1 = new Station("S00333","Agra");
+        Station station2 = new Station("S00444","Lucknow");
+
+        //adding new Trains
+        Train train1 = new Train("12408","Duranto Exp","Express");
+        Train train2 = new Train("12608","Intercity","Passenger");
+
+        Platform p1 = new Platform(1);
+        Platform p2 = new Platform(2);
+
+        p1.setTrain(train1);
+        p2.setTrain(train2);
+
+        List<Platform> platformList = new ArrayList<>();
+        platformList.add(p1);
+        platformList.add(p2);
+
+        station1.setPlatformList(platformList);
+
+        Schedule s1 = new Schedule(train1,station1,"9Am","9:05AM",p1);
+        Schedule s2 = new Schedule(train1,station2,"10Am","10:02AM",p1);
+
+        system.addStations(station1);
+        system.addStations(station2);
+        system.addTrain(train1);
+        system.addTrain(train2);
+        system.addSchedule(s2);
+
+        //lets find train between stations
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the source : ");
+        String source = sc.nextLine();
+        System.out.println("Enter the destination");
+        String des =sc.nextLine();
+        List<Schedule> myTrains = system.findMyTrains(source, des);
+        System.out.println("Your Trains Between "+ source +" and "+des );
+        System.out.println(myTrains);
+
+    }
+}
